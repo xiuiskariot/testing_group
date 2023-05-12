01b8c28c0ba38f6feb6a9fdd75d81f8ac9a0cd6c
Viktor <midyukinv@mail.ru>
2023-05-12 14:39:29 +0300

    add Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-
+import { Header } from "./Header/Header";
 function App() {
   return (
     <div className="App">
+      <Header/>
     </div>
   );
 }
---
+++ src/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import s from './Header.module.css'
+export const Header = () => {
+  return (
+    <>
+      <ul className={s.ul}>
+        <li>Serega</li>
+        <li>Seva</li>
+        <li>Ksyuha</li>
+        <li>Vitya</li>
+    </ul>
+    </>
+  )
+}
